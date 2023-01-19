d4c309bb9487c1c864e3ab6266db92f571051a05
Keagan St Rose <esorts.k990@gmail.com>
2023-01-19 14:28:39 -0700

    working link from arduino to react

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-var index = fs.readFileSync( 'index.html');
+var live = fs.readFileSync( 'app.js');
 
 var SerialPort = require('serialport');
 const parsers = SerialPort.parsers;
@@ -19,30 +19,58 @@ var port = new SerialPort("COM7",{
 
 port.pipe(parser);
 
-var app = http.createServer(function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-res.setHeader('Access-Control-Max-Age', 2592000); // 30 days
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index);
-});
 
 var io = require('socket.io').listen(app);
 
+
+
+
 io.on('connection', function(socket) {
+    console.log(socket)
+    io.on('data', function (data) {console.log(data)});
     
+  
     console.log('Node is listening to port');
     
 });
 
+
+
+
+var liveDataLink = ("")
 parser.on('data', function(data) {
-    
     console.log('Received data from port: ' + data);
-    console.log(data)
-    
     io.emit('data', data);
-    console.log(data)
+    liveDataLink = data;
+
+   
+});
+
+var app = http.createServer(function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+res.setHeader('Access-Control-Max-Age', 2592000); // 30 days
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    
+    res.end(JSON.stringify(liveDataLink))
+    // res.end(JSON.stringify(
+    //     {
+    //       "albumId": 1,
+    //       "id": 1,
+    //       "title": "This is hardest bug ever",
+    //       "url": "www.fixthisbug.com",
+    //       "thumbnailUrl": "https://bughub.com"
+    //     },
+    //     {
+    //       "albumId": 1,
+    //       "id": 2,
+    //       "title": "What is going on here",
+    //       "url": "https://clueless.com/justkidding",
+    //       "thumbnailUrl": "stuff"
+    //     }));
     
 });
+
+
 
 app.listen(3001);
